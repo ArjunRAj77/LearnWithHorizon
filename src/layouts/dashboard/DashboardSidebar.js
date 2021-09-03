a7cf433b40d1dcbbd273fbaba02cc56d47b43285
Arjun Raj <arjunraj.pala@gmail.com>
2021-09-03 13:46:48 +0530

    updated account type

--- src/layouts/dashboard/DashboardSidebar.js
+++ src/layouts/dashboard/DashboardSidebar.js
@@ -71,7 +71,7 @@ export default function DashboardSidebar({ isOpenSidebar, onCloseSidebar }) {
                 {account.displayName}
               </Typography>
               <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                {account.role}
+                {account.accountType}
               </Typography>
             </Box>
           </AccountStyle>
