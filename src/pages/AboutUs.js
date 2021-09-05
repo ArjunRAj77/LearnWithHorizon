ba1c8bdab9fad52ac29ad54e83d6cce2d30ca105
Arjun Raj <arjunraj.pala@gmail.com>
2021-09-05 10:27:37 +0530

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,30 @@
 import * as React from 'react';
 
 export default function AboutUs() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <div style={{ backgroundImage: 'url(/static/illustrations/indexpage.png)' }}>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+        <h1>Welcome to The Inevitables</h1>
+      </div>
+    </>
+  );
 }
